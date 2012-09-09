8359bda4a5abcd6f30c64cff26834d58524228eb
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-09 18:08:26 +0200

    Add entry(Node, Sorted) to dijkstra module

--- src/dijkstra.erl
+++ src/dijkstra.erl
@@ -1,1 +1,21 @@
 -module(dijkstra).
+-export([entry/2]).
+
+% Public functions
+
+% Returns the length of the shortest path to the node
+% 0 if the node is not found
+entry(Node,Sorted) ->
+    lists:foldl(fun({NameNode, Length, Gateway},Acc) -> 
+			if 
+			    % Check if we are searching in a good node
+			    NameNode == Node -> 
+				if
+				    Acc == 0 -> Length;
+				    true -> erlang:min(Length,Acc)
+				end;
+
+			    true -> Acc
+			end
+		end,			 
+	  0, Sorted).

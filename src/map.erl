f1ff0a8f95368a344aff887ff18997f7580c6565
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-09 17:00:01 +0200

    Add function reachable(Node, Map) in Map module

--- src/map.erl
+++ src/map.erl
@@ -1,5 +1,5 @@
 -module(map).
--export([new/0,update/3]).
+-export([new/0,update/3, reachable/2]).
 
 % Public functions %
 
@@ -7,7 +7,15 @@
 new() ->
     [].
 
-% Updates the map to reflect that Node has directional links to all nodes in the list Links
+% Updates the map to reflect that Node has directional
+% links to all nodes in the list Links
 update(Node, Links, Map) ->
     NewMap = lists:keydelete(Node,1,Map),
-    lists:append(NewMap, {Node, Links}).
+    [{Node, Links} | NewMap].
+
+% Returns the list of nodes directly reachable from Node
+reachable(Node, Map) ->
+    case lists:keyfind(Node,1,Map) of
+	false -> [];
+	{_, ListNodes} -> ListNodes
+    end.

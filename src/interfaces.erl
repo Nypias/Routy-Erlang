41edd763b99fae75ce18f9b7c3ff452a1464dd2e
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-11 11:15:09 +0200

    Add function add in interfaces module

--- src/interfaces.erl
+++ src/interfaces.erl
@@ -1,6 +1,13 @@
 -module(interfaces).
--export([new/0]).
+-export([new/0, add/4]).
 
 new() ->
     [].
 
+add(Name, Ref, Pid, Intf) ->
+    case lists:member({Name, Ref, Pid}, Intf) of
+	true ->
+	    Intf;
+	false ->
+	    [{Name, Ref, Pid} | Intf]
+    end.

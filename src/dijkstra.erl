04b12e27889c71d4d2b1260593ba58b7c5325e7e
Thomas FATTAL <thomas.fattal@gmail.com>
2012-09-11 11:06:24 +0200

    Add function route/2 and table/2 to dijkstra module

--- src/dijkstra.erl
+++ src/dijkstra.erl
@@ -1,5 +1,5 @@
 -module(dijkstra).
--export([entry/2, replace/4, update/4]).
+-export([table/2,route/2]).
 
 % Public functions
 
@@ -24,4 +24,47 @@ update(Node, N, Gateway, Sorted) ->
 	N < Length ->
 	    replace(Node,N,Gateway,Sorted);
 	true -> Sorted
+    end.
+
+% Main algorithm for Dijkstra
+iterate(Sorted, Map, Table) ->
+    case Sorted of
+	[] ->
+	    Table;
+	[{_,inf,_} | _] ->
+	    Table;
+	[Entry | Tail] ->
+	    {NodeName,Length,Gateway} = Entry, 
+	    case lists:keyfind(NodeName,1,Map) of
+		{_,Reachables} ->
+		    NewList = lists:foldl(fun(E1,SortedList) ->
+						  update(E1,Length+1,Gateway,SortedList) end, 
+					  Tail, Reachables);
+		false ->
+		    NewList = Tail
+	    end,
+	    iterate(NewList,Map,[{NodeName,Gateway} | Table])
+    end.
+
+% Produce a routing table
+table(Gateways, Map) ->
+    AllNodes = map:all_nodes(Map),
+    InitSortedList = lists:keysort(2, lists:map(fun(Node) ->
+							case lists:member(Node, Gateways) of
+							    true ->
+								{Node, 0, Node};
+							    false ->
+								{Node, inf, unknown}
+							end
+						end, AllNodes)),
+    iterate(InitSortedList, Map, []).
+
+
+% Search the routing table and return the gateway suitable to route messages to a node
+route(Node, Table) ->
+    case lists:keyfind(Node,1,Table) of
+	{Dest, Gateway} ->
+	    {ok, Gateway};
+	false ->
+	    notfound
     end.
